acfcecd9b690c672636dcb7764ca783ff0eee249
Mustafa Elgammal <mustafa.adel.elgammal@gmail.com>
2021-02-25 14:46:34 +0200

    Display all Questions with user information Show debug bar

--- app/User.php
+++ app/User.php
@@ -40,4 +40,7 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
+    public function getUrlAttribute(){
+        return '#';
+    }
 }
